58268360eaa1da4aea0d327903c1759be875ccf1
StarLord <chrisannfalk@gmail.com>
2018-10-13 11:07:19 -0500

    auth additions and tutor-student pages

---
+++ src/Components/Register/Register.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react';
+import "./register.css";
+import axios from 'axios';
+
+
+
+class Register extends Component {
+    constructor(props){
+      super(props);
+        this.state = {
+            fistNameValue: '',
+            lastNameValue: '',
+            emailValue: '',
+            role: 'tutor, student',
+            passwordValue: '',
+        }
+
+        this.handleFirstName = this.handleFirstName.bind(this);
+        this.handleLastName = this.handleLastName.bind(this);
+        this.handleEmail = this.handleEmail.bind(this);
+        this.handleRole = this.handleRole.bind(this);
+        this.handlePasword = this.handlePasword.bind(this);
+        this.submit = this.submit.bind(this);
+        
+    }
+    handleFirstName(event){
+        this.setState({ firstNameValue: event.target.value})
+        console.log(this.state.firstNameValue || '');
+  
+    }
+    handleLastName(event){
+        this.setState({ lastNameValue: event.target.value})
+        console.log(this.state.lastNameValue || '');
+
+    }
+    handleEmail(event){
+        this.setState({ emailValue: event.target.value})
+        console.log(this.state.emailValue || '');
+
+    }
+    handleRole(event){
+        this.setState({ roleValue: event.target.value})
+        console.log(this.state.roleValue || '');
+
+    }
+    handlePasword(event) {
+        this.setState({ passwordValue: event.target.value})
+        console.log(this.state.passwordValue || '');
+    }
+    submit(event) {
+      event.preventDefault()
+      console.log('handleSubmit')
+
+      axios
+          .post('http://localhost:3001/user/register', {
+          firstName: this.state.firstNameValue,
+          lastName: this.state.lastNameValue,
+          username: this.state.emailValue,
+          role: this.state.roleValue,
+          password: this.state.passwordValue
+        })
+        .then(response => {
+            console.log('login response: ')
+            console.log(response)
+            if (response.status === 200) {
+                // update App.js state
+                // this.props.updateUser({
+                //     loggedIn: true,
+                //     username: response.data.username
+                // })
+                // update the state to redirect to home
+                this.setState({
+                    redirectTo: '/'
+                })
+            }
+        }).catch(error => {
+            console.log('login error: ')
+            console.log(error);
+            
+        })
+}
+
+  render() {
+    return (
+      <div >
+       
+        <h1 className="header">Enter your information below to Register</h1>
+        <div className="info">
+             <input value={this.state.firstNameValue} onChange = {this.handleFirstName} placeholder="First Name"/>
+        </div>
+        <div className="info">
+             <input value={this.state.lastNameValue} onChange = {this.handleLastName} placeholder="Last Name"/>
+        </div>
+        <div className="info">
+             <input value={this.state.emailValue} onChange = {this.handleEmail} placeholder="email"/>
+        </div>
+        <div className="info">
+             <input value={this.state.roleValue} onChange = {this.handleRole} placeholder="Tutor or Student"/>
+        </div>
+        <div className="info">
+             <input value={this.state.passwordValue} onChange= {this.handlePasword} placeholder ="password"/>
+        </div>
+        <br></br>
+        <button className="submitButton" onClick={this.submit}>Register</button>
+       
+      </div>
+    );
+  }
+}
+
+export default Register;
