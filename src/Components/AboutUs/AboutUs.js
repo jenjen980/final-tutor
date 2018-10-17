e810f2992c1c14554876100075ee91f8444ca770
Maska <NikitaNadia@outlook.com>
2018-10-16 20:59:47 -0500

    cleanedout

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -8,69 +8,14 @@ import Parallax from "../../NNcomponents/Parallax/Parallax.jsx";
 import landingPageStyle from "../../assets/jss/material-kit-react/views/landingPage";
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
-// import NavBar from '../NavBar';
-
-// import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
-
-// import Background from '../AboutUs/background1.jpg';
-// import "../AboutUs";
-
-// const Hero = props => (
-//   <div className="hero text-center" style={{ backgroundImage: `url(${props.backgroundImage})` }}>
-//     {props.children}
-//   </div>
-// );
-
-// export default Hero;
-
-// var image = {
-//   width: "100%",
-//   height: "300px",
-//   scale: "50%",
-//   backgroundImage: "url(" + Background + ")"
-// }
-
-// const AboutUs = () => {
-//     return (
-//     <div>
-
-//     {/* <img src=""></img> */}
-//       <section style={image}>
-//         <h4 style={{ padding: "50px 50px", textAlign: "center", color: "black", fontSize: "26px", backgroundImage: "../AboutUs/background1.jpg" }}>About Signature Music Teachers</h4>
-//       </section>
-//         <p style={{ textAlign: "center", color: "purple" }}>Signature Music Teachers was founded in October of 2018.
-//           The basis for Signture Music Teachers is to help students select a music tutor that is right for them.
-//           ADD MORE HERE....
-//         </p>
-//       </div>
-
-//     );
-// };
 
 
-// export default AboutUs;
 
-
-const dashboardRoutes = [];
-
-class LandingPage extends React.Component {
+class AboutUS extends React.Component {
   render() {
     const { classes, ...rest } = this.props;
     return (
       <div>
-        {/* <Header
-          color="gray"
-          routes={dashboardRoutes}
-          brand="Signature Music Teacher"
-          rightLinks={<HeaderLinks />}
-          fixed
-          changeColorOnScroll={{
-            height: 20,
-            color: "white"
-          }}
-          {...rest} */}
-        
-
           <Parallax filter image={require("assets/img/background2.jpg")}> 
           <div className={classes.container}>
         
@@ -89,8 +34,9 @@ class LandingPage extends React.Component {
          <Footer />
          </div>
         );
-      };
+  };
 };
 
 
-export default withStyles(landingPageStyle)(LandingPage);
+
+export default withStyles(landingPageStyle)(AboutUS);
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -8,14 +8,14 @@ import Email from "@material-ui/icons/Email";
 import People from "@material-ui/icons/People";
 // core components
 
-
+// import axios from 'axios';
+import axios from 'axios';
 import Footer from "../../NNcomponents/Footer/Footer.jsx";
 import GridContainer from "../../NNcomponents/Grid/GridContainer.jsx";
 import GridItem from "../../NNcomponents/Grid/GridItem.jsx";
 import Button from "../../NNcomponents/CustomButtons/Button.jsx";
 import Card from "../../NNcomponents/Card/Card.jsx";
 import CardBody from "../../NNcomponents/Card/CardBody.jsx";
-// import CardHeader from "../../NNcomponents/Card/CardHeader.jsx";
 import CardFooter from "../../NNcomponents/Card/CardFooter.jsx";
 import CustomInput from "../../NNcomponents/CustomInput/CustomInput.jsx";
 
@@ -23,14 +23,23 @@ import loginPageStyle from "../../assets/jss/material-kit-react/views/loginPage.
 // import { url } from "inspector";
 // import image from "";
 
-class LoginPage extends React.Component {
+class Login extends React.Component {
   constructor(props) {
     super(props);
     // we use this to make the card to appear after the page has been rendered
     this.state = {
-      cardAnimaton: "cardHidden"
+      cardAnimaton: "cardHidden",
+      emailValue: "",
+      passwordValue: "",
+
     };
-  }
+  
+
+  this.handleEmail = this.handleEmail.bind(this);
+  this.handlePasword = this.handlePasword.bind(this);
+  this.submit = this.submit.bind(this);
+  this.register = this.submit.bind(this);
+  };
   componentDidMount() {
     // we add a hidden class to the card and after 700 ms we delete it and the transition appears
     setTimeout(
@@ -40,16 +49,36 @@ class LoginPage extends React.Component {
       700
     );
   }
+
+  handleEmail(event){
+
+    this.setState({ emailValue: event.target.value})
+    console.log(this.state.emailValue);
+
+}
+handlePasword(event) {
+    this.setState({ passwordValue: event.target.value})
+    console.log(this.state.passwordValue);
+}
+submit() { 
+    // Send the email value and password --
+    let userInfo = {
+      username: this.state.emailValue,
+      password: this.state.passwordValue
+    }
+    axios.post('/login', { userInfo })
+    .then(res => {
+      console.log(res);
+      console.log(res.data);
+    })
+}
+
+
   render() {
     const { classes, ...rest } = this.props;
     return (
       <div>
-        {/* <Header
-          absolute
-          color="transparent"
-          brand="Material Kit React"
-          rightLinks={<HeaderLinks />}
-          {...rest} */}
+
         />
         <div
           className={classes.pageHeader}
@@ -64,58 +93,12 @@ class LoginPage extends React.Component {
               <GridItem xs={12} sm={12} md={4}>
                 <Card className={classes[this.state.cardAnimaton]}>
                   <form className={classes.form}>
-                    {/* <CardHeader color="warning" className={classes.cardHeader}> */}
-                      {/* <h4>Login</h4> */}
-                      {/* <div className={classes.socialLine}>
-                        <Button
-                          justIcon
-                          href="#pablo"
-                          target="_blank"
-                          color="transparent"
-                          onClick={e => e.preventDefault()}
-                        >
-                          <i className={"fab fa-twitter"} />
-                        </Button>
-                        <Button
-                          justIcon
-                          href="#pablo"
-                          target="_blank"
-                          color="transparent"
-                          onClick={e => e.preventDefault()}
-                        >
-                          <i className={"fab fa-facebook"} />
-                        </Button>
-                        <Button
-                          justIcon
-                          href="#pablo"
-                          target="_blank"
-                          color="transparent"
-                          onClick={e => e.preventDefault()}
-                        >
-                          <i className={"fab fa-google-plus-g"} />
-                        </Button>
-                      </div> */}
-                    {/* </CardHeader> */}
                     <p className={classes.divider}>LOGIN</p>
                     <CardBody>
                       <CustomInput
-                        labelText="First Name..."
-                        id="first"
-                        formControlProps={{
-                          fullWidth: true
-                        }}
-                        inputProps={{
-                          type: "text",
-                          endAdornment: (
-                            <InputAdornment position="end">
-                              <People className={classes.inputIconsColor} />
-                            </InputAdornment>
-                          )
-                        }}
-                      />
-                      <CustomInput
                         labelText="Email..."
                         id="email"
+                        onChange = {this.handleEmail}
                         formControlProps={{
                           fullWidth: true
                         }}
@@ -131,6 +114,7 @@ class LoginPage extends React.Component {
                       <CustomInput
                         labelText="Password"
                         id="pass"
+                        onChange= {this.handlePasword}
                         formControlProps={{
                           fullWidth: true
                         }}
@@ -147,9 +131,12 @@ class LoginPage extends React.Component {
                       />
                     </CardBody>
                     <CardFooter className={classes.cardFooter}>
-                      <Button simple color="warning" size="lg">
+                    
+                      <Button onClick={this.submit} simple color="warning" size="lg">
+                      
                         Get started
                       </Button>
+
                     </CardFooter>
                   </form>
                 </Card>
@@ -163,70 +150,14 @@ class LoginPage extends React.Component {
   }
 }
 
-export default withStyles(loginPageStyle)(LoginPage);
+export default withStyles(loginPageStyle)(Login);
 
 
 
 
 
-// import React, { Component } from 'react';
-// import "./login.css";
-// import axios from 'axios';
 
 
 
-// class Login extends Component {
-//     constructor(props){
-//       super(props);
-//         this.state = {
-//             emailValue: "",
-//             passwordValue: ""
-//         }
-//         this.handleEmail = this.handleEmail.bind(this);
-//         this.handlePasword = this.handlePasword.bind(this);
-//         this.submit = this.submit.bind(this);
-//         this.register = this.submit.bind(this);
-//     }
-//     handleEmail(event){
 
-//         this.setState({ emailValue: event.target.value})
-//         console.log(this.state.emailValue);
-
-//     }
-//     handlePasword(event) {
-//         this.setState({ passwordValue: event.target.value})
-//         console.log(this.state.passwordValue);
-//     }
-//     submit() { 
-//         // Send the email value and password --
-//         let userInfo = {
-//           username: this.state.emailValue,
-//           password: this.state.passwordValue
-//         }
-//         axios.post('/login', { userInfo })
-//         .then(res => {
-//           console.log(res);
-//           console.log(res.data);
-//         })
-//         }
-
-//   render() {
-//     return (
-//       // <div style={{ textAlign: 'center', backgroundColor: "#999", width: "1000px", minHeight: "200px"}}>
-//       <div>
-//         <h1 className="header">Please enter your email and password</h1>
-//         <div className="login">
-//              <input value={this.state.emailValue} onChange = {this.handleEmail} placeholder="Email"/>
-//         </div>
-//         <div className="login">
-//              <input value={this.state.passwordValue} onChange= {this.handlePasword} placeholder ="password"/>
-//         </div>
-//         <br></br>
-//         {/* <button onClick={this.register}>Register</button>  */}
-//         <button className="submitButton" onClick={this.submit}>Login</button>
-       
-//       </div>
-//     );
-//   }
-// }
 
